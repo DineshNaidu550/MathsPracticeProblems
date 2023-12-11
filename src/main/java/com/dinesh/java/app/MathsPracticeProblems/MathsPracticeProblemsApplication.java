b202f867afe83ce990fb6604224490893fa5a677
DineshNaidu550 <dineshnaidu550@gmail.com>
2023-12-11 11:39:53 +0530

    first commit

---
+++ src/main/java/com/dinesh/java/app/MathsPracticeProblems/MathsPracticeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.java.app.MathsPracticeProblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathsPracticeProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathsPracticeProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinesh/java/app/MathsPracticeProblems/MathsPracticeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.java.app.MathsPracticeProblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathsPracticeProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
