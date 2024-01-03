4ee1a0b02edeb13c77db8a3beabf670fc9e2163c
DineshNaidu550 <125855572+DineshNaidu550@users.noreply.github.com>
2024-01-03 15:57:31 +0530

    Update HashMapExample.java

--- src/main/java/com/dinesh/java/app/collections/HashMapExample.java
+++ src/main/java/com/dinesh/java/app/collections/HashMapExample.java
@@ -3,7 +3,7 @@ package com.dinesh.java.app.collections;
 public class HashMapExample {
 	
 	public static void main(String[] args) {
-		
+		HashMap<String, String> hashMap = new HashMap<>();
 	}
 
 }
