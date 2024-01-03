5e5290ab6da38f2ece0e66dfd780034be4e7a704
DineshNaidu550 <dineshnaidu550@gmail.com>
2024-01-03 15:24:45 +0530

    first commit

---
+++ src/main/java/com/dinesh/java/app/MathsPracticeProblems/Factorials.java
@@ -0,0 +1,19 @@
+package com.dinesh.java.app.MathsPracticeProblems;
+
+import java.util.Scanner;
+
+public class Factorials {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("The given factorial is");
+		int num = sc.nextInt();
+		int factnum = 1;
+		for (int i = 1; i <= num; i++) { //for loop 
+			System.out.println("\t" + i);
+			factnum = factnum * i;
+		}
+			System.out.println("Factorial of " + num + "is" + factnum);
+		
+
+	}
+}
